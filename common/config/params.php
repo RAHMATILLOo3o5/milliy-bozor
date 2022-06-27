6685518da5931105304db065567983bfa3d9fc11
RAHMATILLOo3o5 <96003461+RAHMATILLOo3o5@users.noreply.github.com>
2022-06-27 14:38:25 +0500

    Update params.php

--- common/config/params.php
+++ common/config/params.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'adminEmail' => 'husanboyev05@mail.ru',
-    'supportEmail' => 'husanboyev05@mail.ru',
-    'senderEmail' => 'husanboyev05@mail.ru',
+    'adminEmail' => 'bozor@milliy-bozor.com',
+    'supportEmail' => 'bozor@milliy-bozor.com',
+    'senderEmail' => 'bozor@milliy-bozor.com',
     'senderName' => 'Milliy Bozor',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
