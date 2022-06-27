87de511f4bcdfa3ef143d844414b9d2795660c5e
RAHMATILLOo3o5 <96003461+RAHMATILLOo3o5@users.noreply.github.com>
2022-06-27 14:14:45 +0500

    Update bootstrap.php

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,5 +4,5 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@saveimg', dirname(dirname(__DIR__)) . '/frontend/web/img');
-Yii::setAlias('@defaultImgPath', 'http://milliy-bozor/images');
-Yii::setAlias('@getimg', 'http://milliy-bozor/img');
+Yii::setAlias('@defaultImgPath', 'https://milliy-bozor.com/images');
+Yii::setAlias('@getimg', 'https://milliy-bozor.com/img');
